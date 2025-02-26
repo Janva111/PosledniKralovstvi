87b31930a81baf0780a951a2f6073b445bff1ee3
janva111 <vavraj08@gmail.com>
2025-02-26 16:52:49 +0100

    Complete creating map and prepare for movement

--- src/Command/Console/Console.java
+++ src/Command/Console/Console.java
@@ -13,7 +13,7 @@ public class Console {
     private boolean exit = false;
     private HashMap<String, Command> mapOfCommands = new HashMap<>();
     public static String commandList = "commandList.txt";
-    public void inicializace(){
+    public void inicialization(){
         mapOfCommands.put("jdi argolis", new jdiArgolis());
         mapOfCommands.put("jdi delos", new jdiDelos());
         mapOfCommands.put("jdi lakedaimon", new jdiLakedaimon());
@@ -21,11 +21,12 @@ public class Console {
         mapOfCommands.put("jdi nythrador", new jdiNythrador());
         mapOfCommands.put("jdi solmora", new jdiSolmora());
         mapOfCommands.put("jdi sylvaria", new jdiSylvaria());
+        // add commands
     }
 
     private Scanner scanner = new Scanner(System.in);
 
-    private void proved(){
+    private void make(){
         System.out.print(">>");
         String command = scanner.nextLine();
         command = command.trim();
@@ -35,23 +36,23 @@ public class Console {
             System.out.println(">> "+mapOfCommands.get(command).execute());
             exit = mapOfCommands.get(command).exit();
         }else{
-            System.out.println(">> Nedefinovany prikaz");
+            System.out.println(">> Not define command");
         }
     }
 
     public void start(){
-        inicializace();
+        inicialization();
         try{
             do{
-                proved();
+                make();
             }while(!exit);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
-    private void saveCommad(String prikaz){
+    private void saveCommad(String command){
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(commandList,true))){
-            bw.write(prikaz);
+            bw.write(command);
             bw.newLine();
         }catch(Exception e){
 
