6d72bfea5f24d82ef4d3e5b9a7fdf4b05d8b2fe3
janva111 <vavraj08@gmail.com>
2025-03-20 10:38:26 +0100

    prepare of test classes

---
+++ src/Test/TraderTest.java
@@ -0,0 +1,24 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+class TraderTest {
+
+
+    /*
+        assertNotEquals() - Zkontroluje, zda 2 hodnoty nejsou stejné.
+        assertNotNull() - Zkontroluje, zda hodnota není null.
+        assertNotSame() - Zkontroluje, zda 2 reference neukazují na stejný objekt.
+        assertFalse() - Zkontroluje, zda je hodnota false.
+    */
+
+
+
+    @Test
+    void generateItems() {
+    }
+
+    @Test
+    void findItem() {
+    }
+}
